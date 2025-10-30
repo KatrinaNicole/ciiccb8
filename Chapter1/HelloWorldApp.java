7d904ce0076579d51bf3562e629142854449b902
KatrinaNicole <109775978+KatrinaNicole@users.noreply.github.com>
2025-10-30 03:50:20 +0000

    HelloWorldApp

--- Chapter1/HelloWorldApp.java
+++ Chapter1/HelloWorldApp.java
@@ -5,7 +5,7 @@
  * The HelloWorldApp class implements an application that
  * simply prints "Hello World!" to standard output.
  */
-public class helloWorldApp {
+public class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Kumusta Mundo!"); // Display the string.
     }
