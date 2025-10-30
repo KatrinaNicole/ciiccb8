55869b0ab116b19913697b1b3bbba3e57a16ca52
KatrinaNicole <109775978+KatrinaNicole@users.noreply.github.com>
2025-10-30 03:39:48 +0000

    helloWorldApp

---
+++ Chapter1/helloWorldApp.java
@@ -0,0 +1,12 @@
+//Name: Abrio, Katrina Nicole
+//Task#: 1-1-1
+//----------------------------
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+public class helloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Kumusta Mundo!"); // Display the string.
+    }
+}
