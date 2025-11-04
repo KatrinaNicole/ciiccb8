bf9dd3e3e5046b0b74dca0bfddc241884a76fb09
KatrinaNicole <109775978+KatrinaNicole@users.noreply.github.com>
2025-11-04 14:44:37 +0000

    Task2

---
+++ TASKS/Task2.java
@@ -0,0 +1,22 @@
+//Name: Abrio, Katrina Nicole
+//Task#: 2
+public class Task2 {
+    public static void main(String[] args) {
+
+        byte zero = 0;
+        short one = 1;
+        int three = 3;
+        float four = 4.0f;   // using float
+        char H = 'H';
+        char w = 'w';
+        char r = 'r';
+        char d = 'd';
+        boolean t = true;
+
+        String output = "" + H + three + one + one + zero + " " + w + zero + r + one + d + " " + t;
+
+        System.out.println(output);
+        
+    }
+
+}
