9ead3e20fe66306c5e21d798433bddfe041e191a
KatrinaNicole <109775978+KatrinaNicole@users.noreply.github.com>
2025-11-06 09:04:55 +0000

    Task5

--- TASKS/Task5.java
+++ TASKS/Task5.java
@@ -9,7 +9,6 @@ public class Task5 {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter 3 numbers separated by spaces: ");
         int a = input.nextInt(), b = input.nextInt(), c = input.nextInt();
-        input.close();
 
         String result = (a==b && b==c) ?
             "All numbers are equal" : (a>b && a>c) ?
